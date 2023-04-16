9debedd8712ad4e33fe08286802f3830ac2f3266
akshay_manthekar <akky123654@gmail.com>
2023-04-16 15:20:52 +0530

    Added Button to move between levels

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
     public Button button;
+    public int totalLevels = 3;
+    private int currentScene;
     // Start is called before the first frame update
     void Start()
     {
+        // Storing the current scene index
+        currentScene = SceneManager.GetActiveScene().buildIndex;
         button.onClick.AddListener(OnButtonClick);
     }
 
     void OnButtonClick()
     {
-        Debug.Log("Button Clicked\n");
+        Debug.Log("Button Clicked\nMoving to Next Level");
+        if(currentScene == totalLevels)
+        {
+            Debug.Log("Coming back to the Main Menu...\n");
+            SceneManager.LoadScene(0);  
+        } 
+        else
+        {
+            SceneManager.LoadScene(currentScene + 1);
+        } 
     }
 
     // Update is called once per frame

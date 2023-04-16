b11c6e1a1dbd88b6505c2384d0cb8d5bf574314d
akshay_manthekar <akky123654@gmail.com>
2023-04-17 00:16:27 +0530

    Final Commit

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,34 +6,38 @@ using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    public Button button;
-    public int totalLevels = 3;
-    private int currentScene;
+    public Button L1_button;
+    public Button L2_button;
+    public Button L3_button;
+    public Button backButton;
     // Start is called before the first frame update
     void Start()
     {
+        /*L1_button= gameObject.GetComponent<Button>();
+        L2_button= gameObject.GetComponent<Button>();
+        L3_button= gameObject.GetComponent<Button>();*/
+
         // Storing the current scene index
-        currentScene = SceneManager.GetActiveScene().buildIndex;
-        button.onClick.AddListener(OnButtonClick);
+        if(L1_button!=null) { L1_button.onClick.AddListener(LoadLevel1); }
+        if (L2_button != null) { L2_button.onClick.AddListener(LoadLevel2); }
+        if (L3_button != null) { L3_button.onClick.AddListener(LoadLevel3); }
+        if (backButton != null) { backButton.onClick.AddListener(ReturnToMain); }
     }
 
-    void OnButtonClick()
+    public void LoadLevel1()
     {
-        Debug.Log("Button Clicked\nMoving to Next Level");
-        if(currentScene == totalLevels)
-        {
-            Debug.Log("Coming back to the Main Menu...\n");
-            SceneManager.LoadScene(0);  
-        } 
-        else
-        {
-            SceneManager.LoadScene(currentScene + 1);
-        } 
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void LoadLevel2()
     {
-        
+        SceneManager.LoadScene(2);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
